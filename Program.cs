8fa2532b7a62185cb347a3628de33a84d7fc5fc2
keckluis <keckluis@gmail.com>
2020-01-18 11:55:03 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Softwaredesign_Abschlussaufgabe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
